0f2b3369e74f40e4fd389188bb073adb19a6948d
BugHunter-X <askzenai@gmail.com>
2025-02-03 23:54:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,14 @@
+function myFunction(a, b) {
+  if (a === 0) {
+    return b; 
+  }
+  if (b === 0) {
+    return a;
+  }
+  return myFunction(Math.min(a, b), Math.abs(a - b));
+}
+
+console.log(myFunction(10, 6)); // Output: 2
+console.log(myFunction(48, 18)); // Output: 6
+console.log(myFunction(0, 10)); // Output: 10
+console.log(myFunction(10, 0)); // Output: 10
